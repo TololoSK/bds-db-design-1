5e234ef2c8589d3d332ca6748a7e0a635cac3792
TololoSK <92521616+TololoSK@users.noreply.github.com>
2021-11-15 21:16:53 +0100

    Update V1__create_tables.sql
    Added ON DELETE CASCADE to where it makes sense

--- sql/V1__create_tables.sql
+++ sql/V1__create_tables.sql
@@ -9,13 +9,13 @@ CREATE TABLE passenger (
 	first_name VARCHAR(45) NOT NULL,
 	surname VARCHAR(45) NOT NULL,
 	id_contact INT NOT NULL,
-	FOREIGN KEY (id_contact) REFERENCES contact (id_contact)
+	FOREIGN KEY (id_contact) REFERENCES contact (id_contact) ON DELETE SET NULL
 );
 CREATE TABLE luggage(
 	id_luggage ${autoincrement},
 	weight FLOAT NOT NULL,
 	id_passenger INT NOT NULL,
-	FOREIGN KEY (id_passenger) REFERENCES passenger (id_passenger)
+	FOREIGN KEY (id_passenger) REFERENCES passenger (id_passenger) ON DELETE CASCADE
 );
 CREATE TABLE address(
 	id_address ${autoincrement},
@@ -28,8 +28,8 @@ CREATE TABLE address(
 CREATE TABLE passenger_has_address(
 	id_address INT NOT NULL,
 	id_passenger INT NOT NULL,
-	FOREIGN KEY (id_address) REFERENCES address (id_address),
-	FOREIGN KEY (id_passenger) REFERENCES passenger (id_passenger)
+	FOREIGN KEY (id_address) REFERENCES address (id_address) ON DELETE CASCADE,
+	FOREIGN KEY (id_passenger) REFERENCES passenger (id_passenger) ON DELETE CASCADE
 );
 CREATE TABLE employee_type(
 	id_employee_type ${autoincrement},
@@ -73,21 +73,21 @@ CREATE TABLE flight(
 	gate VARCHAR(45) NOT NULL,
 	departure_time TIMESTAMP NOT NULL,
 	id_aircraft INT NOT NULL,
-	FOREIGN KEY (id_aircraft) REFERENCES aircraft (id_aircraft)
+	FOREIGN KEY (id_aircraft) REFERENCES aircraft (id_aircraft) ON DELETE CASCADE
 );
 CREATE TABLE flight_crew(
 	id_flight INT NOT NULL,
 	id_employee INT NOT NULL,
-	FOREIGN KEY (id_flight) REFERENCES flight (id_flight),
-	FOREIGN KEY (id_employee) REFERENCES employee (id_employee)
+	FOREIGN KEY (id_flight) REFERENCES flight (id_flight) ON DELETE CASCADE,
+	FOREIGN KEY (id_employee) REFERENCES employee (id_employee) ON DELETE CASCADE
 );
 CREATE TABLE ticket(
 	id_passenger ${autoincrement},
 	id_ticket_type INT NOT NULL,
 	id_flight INT NOT NULL,
 	id_airline INT NOT NULL,
-	FOREIGN KEY (id_passenger) REFERENCES passenger (id_passenger),
-	FOREIGN KEY (id_ticket_type) REFERENCES ticket_type (id_ticket_type),
-	FOREIGN KEY (id_flight) REFERENCES flight (id_flight),
-	FOREIGN KEY (id_airline) REFERENCES airline (id_airline)
+	FOREIGN KEY (id_passenger) REFERENCES passenger (id_passenger) ON DELETE CASCADE,
+	FOREIGN KEY (id_ticket_type) REFERENCES ticket_type (id_ticket_type) ON DELETE CASCADE,
+	FOREIGN KEY (id_flight) REFERENCES flight (id_flight) ON DELETE CASCADE,
+	FOREIGN KEY (id_airline) REFERENCES airline (id_airline) ON DELETE CASCADE
 );
