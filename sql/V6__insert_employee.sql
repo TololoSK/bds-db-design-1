5962d14901048fbd1bdc2cda884a365fb325dec5
TololoSK <92521616+TololoSK@users.noreply.github.com>
2021-11-15 21:30:06 +0100

    Update V6__insert_employee.sql
    changed password and email

--- sql/V6__insert_employee.sql
+++ sql/V6__insert_employee.sql
@@ -1,7 +1,7 @@
-INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Chauncey', 'Pohlman', 'iuahfheauf', 'Chauncey.Pohlman@gmail.com', 1);
-INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Rodney', 'Miller', 'ishgfshapowkqw', 'Rodney.Miller@gmail.com', 2);
-INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Roger', 'King', 'uhyugqgqnd', 'Roger.King@gmail.com', 3);
-INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Cinthia', 'Thompson', 'yagwdanjlskdA','Cinthia.Thompson@gmail.com', 1);
-INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Patricia', 'Murray', 'qwuijdadslk', 'Patricia.Murray@gmail.com', 5);
-INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Michaela', 'Spok', 'ishgafsfshapowkqw', 'Michaela.Spok@gmail.com', 2);
-INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Larry', 'King', 'uhyugqgqnd', 'Larry.King@gmail.com', 3);
+INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Chauncey', 'Pohlman', 'Chauncey.Pohlman@gmail.com', 'iuahfheauf', 1);
+INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Rodney', 'Miller', 'Rodney.Miller@gmail.com', 'ishgfshapowkqw', 2);
+INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Roger', 'King', 'Roger.King@gmail.com', 'uhyugqgqnd', 3);
+INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Cinthia', 'Thompson', 'Cinthia.Thompson@gmail.com','yagwdanjlskdA', 1);
+INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Patricia', 'Murray', 'Patricia.Murray@gmail.com', 'qwuijdadslk', 5);
+INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Michaela', 'Spok', 'Michaela.Spok@gmail.com', 'ishgafsfshapowkqw', 2);
+INSERT INTO employee(first_name, surname, email, password, id_employee_type) VALUES('Larry', 'King', 'Larry.King@gmail.com', 'uhyugqgqnd', 3);
